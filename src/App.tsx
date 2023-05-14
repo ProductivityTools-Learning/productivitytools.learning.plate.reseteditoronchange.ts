31116873cd2c444cf15c74ded1eec20acaa084cd
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-14 20:26:35 +0200

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import { Plate, useResetPlateEditor } from "@udecode/plate";
+import { MyValue } from "./typescript/plateTypes";
+
+const ResetEditorOnValueChange = ({ value }: { value: MyValue }) => {
+  const resetPlateEditor = useResetPlateEditor();
+  const isFirst = useRef(true);
+
+  useEffect(() => {
+    if (isFirst.current) {
+      isFirst.current = false;
+      return;
+    }
+
+    resetPlateEditor();
+  }, [value, resetPlateEditor]);
+
+  return null;
+};
+
 
 function App() {
   return (
