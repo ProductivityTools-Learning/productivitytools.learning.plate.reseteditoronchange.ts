c93a4edf82a831d80796b37559e8c5eb76292d4f
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-15 06:37:43 +0200

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Plate, useResetPlateEditor } from "@udecode/plate";
 import { plainTextValue } from "./basic-editor/plainTextValue";
 
 import { editableProps } from "./common/editableProps";
-import { MyValue } from "./typescript/plateTypes";
+import { MyParagraphElement, MyValue } from "./typescript/plateTypes";
 
 const ResetEditorOnValueChange = ({ value }: { value: MyValue }) => {
   const resetPlateEditor = useResetPlateEditor();
   const isFirst = useRef(true);
-
+  console.log("useffect2");
   useEffect(() => {
     if (isFirst.current) {
+      console.log("useffect1");
       isFirst.current = false;
       return;
     }
@@ -21,15 +22,27 @@ const ResetEditorOnValueChange = ({ value }: { value: MyValue }) => {
   return null;
 };
 
+const initialValue = (content: string) => [
+  {
+    type: "p",
+    children: [
+      {
+        text: content,
+      },
+    ],
+  } as MyParagraphElement,
+];
+
 function App() {
-  const [debugValue, setDebugValue] = useState<MyValue>(plainTextValue);
+  const [debugValue, setDebugValue] = useState<MyValue>(initialValue("fdsa"));
 
   return (
     <div className="App">
       hello
       <div>
-        <Plate<MyValue> editableProps={editableProps} initialValue={debugValue} onChange={setDebugValue} />
-        <Plate<MyValue> editableProps={{ readOnly: true, ...editableProps }} value={debugValue}>
+        Input which changes Plate2<input type="text" onChange={() => setDebugValue(initialValue("ddd"))}></input><br/><br/>
+        Plate1 changes Plate2 on chnge<Plate<MyValue> editableProps={editableProps} initialValue={debugValue} onChange={setDebugValue} /><br/>
+        Plae2: which is changed but also allows to write in it<Plate<MyValue> editableProps={{ ...editableProps }} value={debugValue}>
           <ResetEditorOnValueChange value={debugValue} />
         </Plate>
       </div>
